400a633535b3299f9ffe10cac9719f72566ac3ec
ivan-diep <ivandiep0730@hotmail.se>
2018-11-20 19:14:25 +0100

    Läxa Vecka 46
    Läxa vecka 46

---
+++ Homework/V.46/v46/LaxaNr4.java
@@ -0,0 +1,23 @@
+package v46;
+
+public class LaxaNr4 {
+
+
+	   public static void main(String[] args) {
+		
+		 
+		   
+	        for(int i = 1; i < 10000; i++) {
+	        	
+	        	if(i % 7== 0) {
+	        		System.out.println(i);
+	        	}
+			   
+			  
+			   
+		   }
+		   
+		}
+		
+	}
+
---
+++ Homework/V.46/v46/LaxaNr5.java
@@ -0,0 +1,17 @@
+package v46;
+
+public class LaxaNr5 {
+
+	 public static void main(String[] args) {
+	
+		 String xchar = "x"; //man har ("") för att det är characters
+		 for(int x = 1; x <= 4; x++) {
+		 
+		 
+		 System.out.println(xchar);
+		 xchar = xchar + "x";
+		 
+	 }
+	 
+}
+}
